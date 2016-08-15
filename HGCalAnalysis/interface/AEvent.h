103105ad09590e12732a159da574b105e6501dd3
kchang2 <kchang2@caltech.edu>
2016-08-15 14:20:28 -0700

    update

--- HGCalAnalysis/interface/AEvent.h
+++ HGCalAnalysis/interface/AEvent.h
@@ -11,12 +11,10 @@ class AEvent : public TObject
 {
 public:
 
-  AEvent(): run(0), evn(0), ngen(0), nhit(0), nclus2d(0),nclus3d(0),
-	    vx(0.), vy(0.), vz(0.)
+  AEvent(): run(0), evn(0), nhit(0), vx(0.), vy(0.), vz(0.)
   {
   }
-  void set(int i_run, int i_event,
-	   int i_ngen, int i_nhit, int i_nclus, int i_nmclus,
+  void set(int i_run, int i_event, int i_nhit,
 	   float i_vx,float i_vy,float i_vz)
   {
     run =   i_run;
@@ -33,10 +31,7 @@ public:
 
   unsigned int run;
   unsigned int evn;
-  int   ngen;
   int   nhit;
-  int   nclus2d;
-  int   nclus3d;
   float vx;
   float vy;
   float vz;
