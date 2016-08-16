9ac245daece95fef0d347fbc68a78a4c1b94da4f
kchang2 <kchang2@caltech.edu>
2016-08-16 03:11:38 -0700

    fixed script

--- HGCalAnalysis/test/script.py
+++ HGCalAnalysis/test/script.py
@@ -14,23 +14,29 @@ import fileinput
 ## PARAMETERS YOU FILL IN ##
 file_dir	= './deep-learning/data/neutrinos.list'
 name		= 'pdg12_pt35'
+dirname		= 'neutrino_pt35'
 
 # open and create neutrino list file
 nfile = open(file_dir)
 lines = nfile.readlines()
 nfile.close()
 
+# checks to see if folder directory exists
+
+if not os.path.exists(dirname):
+	os.makedirs(dirname)
+
 for line in lines:
 	f = open('particle_nosmear_calib.py', 'r')
 	oldfile = f.read()
 	f.close()
 
-	newfile = oldfile.replace("eos/to/reco/root", line)
+	newfile = oldfile.replace("eos/to/reco/root", line[:-1])
 
 	if line[-9] != '_': #single digit 
 		newfile = newfile.replace("rootname", name + "_" + str(line[-7]) + ".root")
 	else:
-		newfile = newfile.replace("rechit_unformatted.npy", name + "_" + str(line[-8:-6]) + ".root")
+		newfile = newfile.replace("rootname", name + "_" + str(line[-8:-6]) + ".root")
 
 	f = open('particle_nosmear_calib.py', 'w')
 	f.write(newfile)
@@ -43,3 +49,6 @@ for line in lines:
 	f = open('particle_nosmear_calib.py', 'w')
 	f.write(oldfile)
 	f.close()
+
+cmd = 'mv %s* ./%s/' %(name, dirname)
+os.system(cmd)
